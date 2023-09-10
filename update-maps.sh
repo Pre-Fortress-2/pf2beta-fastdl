6d2a4af8da1a4bb148b5dbc3317c52cc0508887d
dangreene0 <35577693+dangreene0@users.noreply.github.com>
2023-09-10 00:44:07 -0400

    removed leftover scripting

--- update-maps.sh
+++ update-maps.sh
@@ -10,11 +10,5 @@ for i in *.bz2; do
 done
 
 mv *.bsp *.res *.txt ../../../maps
-cd ~
 
-# rcon-cli can be found at https://github.com/itzg/rcon-cli place it in your home directory
-./rcon-cli --host ip --password rconpw --port 27015 say "(ATTENTION)"
-sleep 1s
-./rcon-cli --host ip --password rconpw --port 27015 say "Custom maps have been updated"
 
-cd servers/sdk/
