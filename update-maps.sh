1b336806906f4a5607cdcf0fdbf101f139b1de21
dani <sourdanidev@gmail.com>
2025-06-06 23:32:59 -0400

    feat: added scripting from pf2 fastdl

--- update-maps.sh
+++ update-maps.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 git pull
-git checkout pf2beta/maps
-cd pf2beta/maps
+git checkout pf2beta
 
+cd pf2beta/maps
 for i in *.bz2; do
     [ -f "$i" ] || break
     bzip2 -d $i
 done
 
-mv *.bsp *.res *.txt ../../../maps
+# Goes to pf2beta/download/maps
+mv *.bsp ../../../maps
 
+# Has to go to pf2beta/maps for now
+cp *.res *.txt ../../../../maps
+cd ..
 
+rm -rf ../../materials
+cp -r materials/ ../../
+
+rm -rf ../../models
+cp -r models/ ../../
+
+cd ..
+cp mapcycle.txt motd.txt ../../cfg
